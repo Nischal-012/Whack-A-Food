79537cf0c3413b18dbd885d931e078ec0e0f266d
Nischal Joshi <076bct051@ioepc.edu.np>
2023-02-16 20:43:11 +0545

    Game

---
+++ Assets/Scripts/GameManagerX.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class GameManagerX : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI  timerText;
+    public GameObject gameOver;
+    public GameObject titleScreen;
+    public Button restartButton; 
+
+    public List<GameObject> targetPrefabs;
+
+    private int score;
+    private float spawnRate = 1.5f;
+    public bool isGameActive;
+    public float timer = 60;
+
+    private float spaceBetweenSquares = 2.5f; 
+    private float minValueX = -3.75f; 
+    private float minValueY = -3.75f;
+
+	private void Update()
+	{
+		if(isGameActive)
+		{
+            timer -= Time.deltaTime;
+            timerText.text = "Timer:" + Mathf.RoundToInt(timer).ToString();
+        }
+	}
+	public void StartGame(int difficultyLevel)
+    {
+        spawnRate /= difficultyLevel;
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+        titleScreen.SetActive(false);
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targetPrefabs.Count);
+
+            if (isGameActive)
+            {
+                Instantiate(targetPrefabs[index], RandomSpawnPosition(), targetPrefabs[index].transform.rotation);
+            }
+            
+        }
+    }
+
+    Vector3 RandomSpawnPosition()
+    {
+        float spawnPosX = minValueX + (RandomSquareIndex() * spaceBetweenSquares);
+        float spawnPosY = minValueY + (RandomSquareIndex() * spaceBetweenSquares);
+
+        Vector3 spawnPosition = new Vector3(spawnPosX, spawnPosY, 0);
+        return spawnPosition;
+
+    }
+
+    int RandomSquareIndex()
+    {
+        return Random.Range(0, 4);
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "score:" + score;
+    }
+
+    public void GameOver()
+    {
+        gameOver.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    } 
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
